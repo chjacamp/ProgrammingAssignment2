333c290db204c022fdbe9a3d9ceaba9ae81b757c
chjacamp <chjacamp@gmail.com>
2015-09-26 18:46:24 -0600

    Added a short intro for what the functions are supposed to do and a synopsis of makeCacheMatrix.

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,12 @@
-## Let's see this commit/push
-
-## Write a short comment describing this function
+## The two functions here - makeCacheMatrix and cacheSolve - attempt to find
+## the inverse of a given matrix and cache the solution for future use. 
+##
+## makeCacheMatrix is a 'factory' function that stores a list of functions: 
+## 'set' stores a matrix
+## 'get' retrieves matrix
+## 'setinvmatrix' sets the solution for the matrix and informs cacheSolve to
+##  retrieve this matrix from cache.
+## 'getinvmatrix' retrieves the matrix cached by setinvmatrix.
 
 makeCacheMatrix <- function(x = matrix()) {
 
